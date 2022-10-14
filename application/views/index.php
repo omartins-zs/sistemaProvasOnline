04ad567178bed53d2258270af0ebedf9e8d01187
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-14 09:32:09 -0300

    Add button de cadastrar pergunta na Inicial

--- application/views/index.php
+++ application/views/index.php
@@ -1,8 +1,13 @@
 <h2>Quiz de conhecimento sobre PHP</h2>
 <p>É um teste de múltipla escolha</p>
 <ul>
-    <li> <strong> Número de preguntas:</strong> 5 </li>
+    <li> <strong> Número de preguntas:</strong> <?= $count_questoes ?> </li>
     <li> <strong> Tipo: </strong> Escolhas múltiplas </li>
-    <li> <strong> Tempo Estimado : </strong> 1 Minuto(s) por pergunta</li>
+    <li> <strong> Tempo Estimado : </strong><?= $count_questoes * .5 ?> Minuto(s) por pergunta</li>
 </ul>
-<a href="<?= base_url('quiz/question/1'); ?>" class="btn btn-outline-primary"> Começar</a>
+
+<div class="d-flex justify-content-between mr-3">
+    <a href="<?= base_url('quiz/questao/1') ?>" class="btn btn-sm btn-primary"> Começar</a>
+
+    <a href="<?= base_url('quiz/add') ?>" class="btn btn-sm btn-success"><i class="fa fa-question-circle mr-1"></i>Cadastrar pergunta</a>
+</div>
