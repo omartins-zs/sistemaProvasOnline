c76ef48021fe66a4d5f23aaf67aa4b417f1cb8d9
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-14 09:33:52 -0300

    Add view de Finalização do Quiz

---
+++ application/views/finalizacao.php
@@ -0,0 +1,6 @@
+<h2>Pronto, acabou!</h2>
+<p>Parabéns! você concluiu o quiz</p>
+
+<p>Pontuação final: <?= $this->session->userdata('pontuacao') ?></p>
+
+<a href="<?= base_url() ?>" class="btn btn-outline-primary">Começar denovo</a>
